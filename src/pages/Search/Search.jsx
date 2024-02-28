9071abdcb065da44ce28ec061d3bb361a92eeee6
enzoshiotuqui <enzomshiotuqui@gmail.com>
2024-02-28 13:03:18 -0300

    feat: search module

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -4,16 +4,29 @@ import styles from './Search.module.css'
 // hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
-
+import PostDetail from '../../components/PostDetail/PostDetail'
+import { Link } from 'react-router-dom'
 const Search = () => {
 
     const query = useQuery()
     const search = query.get('q')
 
+    const {documents: posts} = useFetchDocuments("posts", search)
+
   return (
-    <div>
+    <div className={styles.search_container}>
         <h2>Search</h2>
-        <p>{search}</p>
+        <div>
+            {posts && posts.lengh === 0 && (
+                <>
+                    <p>Não foram encontrados posts a partir da sua busca...</p>
+                    <Link className='btn btn-dark' to="/"></Link>
+                </> 
+            )}
+            {posts && posts.map((post) => (
+                <PostDetail key={post.id} post={post}/>
+            ))}
+        </div>
     </div>
   )
 }
