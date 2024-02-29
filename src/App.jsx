669309e12dafbb23ea0417ba2a581e6dee3c2832
enzoshiotuqui <enzomshiotuqui@gmail.com>
2024-02-29 14:44:59 -0300

    feat: edit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search'
 import Post from './pages/Post/Post'
+import EditPost from './pages/EditPost/EditPost'
+
 
 // Components
 import Navbar from './components/Navbar/Navbar'
@@ -56,6 +58,7 @@ function App() {
                         <Route path='/Posts/:id' element={<Post/>}/>
                         <Route path='/Login' element={!user ? <Login/> : <Navigate to="/Home"/> }/>
                         <Route path='/Register' element={!user ? <Register/> : <Navigate to="/Home"/>}/>
+                        <Route path='/Posts/Edit/:id' element={user ? <EditPost/> : <Navigate to="/Login"/>}/>
                         <Route path='/Posts/Create' element={user ? <CreatePost/> : <Navigate to="/Login"/>}/>
                         <Route path='/Dashboard' element={user ? <Dashboard/> : <Navigate to="/Login"/>}/>
                     </Routes>
